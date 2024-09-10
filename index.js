d2fb80b5df25b3cfc2177cc49031568e1a581b18
ALNoman78 <abdullaalno078@gmail.com>
2024-09-10 23:30:04 +0600

    full assignment completed

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
-// problem - 01
-
 function calculateTax(monthlyIncome, monthlyExpense) {
-  if (
-    monthlyIncome < 0 ||
-    monthlyExpense < 0 ||
-    monthlyIncome < monthlyExpense
-  ) {
-    return "Invalid Input";
-  }
-  let income = monthlyIncome;
-  let expense = monthlyExpense;
-  let remainingAmount = income - expense;
-  let taxAmount = remainingAmount * 0.2;
-  return taxAmount;
-}
-const totalIncome = calculateTax(34000, 1753);
-console.log(totalIncome);
-
-//  problem - 02
-
-function sendNotification(email) {
-  if (email.indexOf("@") === -1) {
-    return "Invalid Email";
-  }
-  let convertArr = email.split("@");
-  let b =
-    convertArr[0] +
-    " sent you an email from " +
-    convertArr[convertArr.length - 1];
-  return b;
-}
-const userEmail = "nadim.naem5@outlook.com";
-const notification = sendNotification(userEmail);
-console.log(notification);
-
-// problem - 03
-
-// function checkDigitsInName(name) {
-//     let number = [1,2,3,4,5,6,7,8,9,0]
-//     let result = false;
-//     for(let i = 0; i < name.length ; i++){
-//         if (name[i] === number) {
-//             result= true;
-//         }
-//     }
-//     return false;
-// }
-// const userName = "Raj123"
-// const checkUserName = checkDigitsInName(userName)
-// console.log(checkUserName);
-
-//  problem - 04
-
-function calculateFinalScore(score) {
-    if (typeof score !== "object") {
+    if (
+        monthlyIncome < 0 ||
+        monthlyExpense < 0 ||
+        monthlyIncome < monthlyExpense
+    ) {
         return "Invalid Input";
     }
-    if (typeof score.testScore !== "number" || typeof score.schoolGrade !== "number" || typeof score.isFFamily !== "boolean") {
+    let income = monthlyIncome;
+    let expense = monthlyExpense;
+    let remainingAmount = income - expense;
+    let taxAmount = remainingAmount * 0.2;
+    return taxAmount;
+}
+
+
+
+
+function sendNotification(email) {
+    if (email.indexOf("@") === -1) {
+        return "Invalid Email";
+    }
+    let convertArr = email.split("@");
+    let b =convertArr[0] + " sent you an email from " + convertArr[convertArr.length - 1];
+    return b;
+}
+
+
+
+function checkDigitsInName(name) {
+    if (typeof name !== "string") {
+        return "Invalid Input";
+    }
+    for(let i = 0; i < name.length ; i++){
+        if(!isNaN(name[i]) && name[i] !== ""){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+function calculateFinalScore(obj) {
+    if (typeof obj !== "object") {
+        return "Invalid Input";
+    }
+    if (typeof obj.testScore !== "number" || typeof obj.schoolGrade !== "number" || typeof obj.isFFamily !== "boolean") {
         return "Invalid Input"
     }
-    if (score.testScore < 0 || score.testScore >= 50 || score.schoolGrade < 0 || score.schoolGrade >= 30) {
+    if (obj.testScore >= 50 || obj.schoolGrade >= 30) {
         return "Invalid Input"
     }
-    let totalScore = score.testScore + score.schoolGrade;
-    if (score.isFFamily === true) {
+    let totalScore = obj.testScore + obj.schoolGrade;
+    if (obj.isFFamily === true) {
         totalScore += 20;
     } else {
         return false;
     }
     return totalScore >= 80;
 }
-const obj = { name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : false };
-const x = calculateFinalScore(obj);
-console.log(x);
 
 
-// problem - 05
+
+
+function  waitingTime(timesArray,serialNumber){
+    if (Array.isArray(timesArray) !== true|| typeof serialNumber !== "number") {
+        return "Invalid Input";
+    }
+    let sum = 0;
+    let avg = timesArray.length
+    for (const e of timesArray) {
+        sum += e;
+    }
+    let avgTime = sum / avg;
+    let serialNumberBefore = serialNumber - avg - 1;
+    let waitingTime = serialNumberBefore * Math.round(avgTime);
+    return waitingTime;
+}
