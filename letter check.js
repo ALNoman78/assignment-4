43b47a34a26e581947f07716b72a11e1703a85d1
ALNoman78 <abdullaalno078@gmail.com>
2024-09-11 15:39:15 +0600

    letter js file create

---
+++ letter check.js
@@ -0,0 +1,16 @@
+function checkDigitsInName(name) {
+    if (typeof name !== "string") {
+        return "Invalid Input";
+    }
+
+    for (let i = 0; i < name.length; i++) {
+        if (!isNaN(name[i]) && name[i] !== "") {
+        return true;
+        }
+    }
+
+    return false;
+}
+console.log(checkDigitsInName("n2oman"));
+
+//if name + number = string function will print boolean value tru
--- new.js
+++ new.js
@@ -4,7 +4,11 @@ function checkDigitsInName(name) {
     }
     let arr = ["0","1","2","3","4","5","6","7","8","9"]
     for(let i = 0; i < arr.length ; i++){
-        console.log(arr[i]);
+        if(name.includes(arr[i])){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 const name = "raju123"
