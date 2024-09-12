91e8b1cb07accb59d4028e8dd17fff7453e1e06d
ALNoman78 <abdullaalno078@gmail.com>
2024-09-12 18:03:50 +0600

    update file

--- new.js
+++ new.js
@@ -4,7 +4,7 @@ function checkDigitsInName(name) {
     }
     let arr = ["0","1","2","3","4","5","6","7","8","9"]
     for(let i = 0; i < arr.length ; i++){
-        if(name.includes(arr[i])){
+        if(!isNaN(arr[i] && arr[i] !== "")){
             return true;
         }
     }
