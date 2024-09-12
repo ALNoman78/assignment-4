ba239dab81f2c057f6454dea149c7815183ec8d3
ALNoman78 <abdullaalno078@gmail.com>
2024-09-12 23:39:39 +0600

    practice session start

--- assignment.js
+++ assignment.js
@@ -29,3 +29,30 @@ console.log(value);
 
 
 // problem no - 03
+
+function checkDigitsInName(name){
+    for(let i = 0; i < name.length ; i++){
+        if (!isNaN(name[i]) && name[i] !== " ") {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(checkDigitsInName("Abdullah!23"));
+
+
+// problem - 04
+
+
+function calculateFinalScore(obj){
+    let totalScore = obj.testScore + obj.schoolGrade;
+    if(obj.isFFamily === true){
+        totalScore += 20;
+    }else{
+        return false;
+    }
+    return totalScore >= 80;
+}
+const newObj =  {name: "Rajib", testScore:45, schoolGrade:25,isFFamily:true };
+const objectPrint = calculateFinalScore(newObj)
+console.log(objectPrint);
